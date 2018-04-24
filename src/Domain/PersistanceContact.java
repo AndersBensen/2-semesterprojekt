7b4e840743dc37a14aef59039413e7d4a54ffc02
Loc Hoang Thanh Nguyen <32257138+LHTNGUYEN@users.noreply.github.com>
2018-04-24 17:15:28 +0200

    Created method getCase and getCaseRequest

--- src/Domain/PersistanceContact.java
+++ src/Domain/PersistanceContact.java
@@ -63,8 +63,63 @@ public class PersistanceContact {
         return this.writer;
     }
     
+    public Case getCase(int ID){
+        String[] c = reader.getCase(ID);
+        Case currentCase = new Case(Integer.parseInt(c[0]), Integer.parseInt(c[1]));
+        
+        currentCase.setCitizenIsInformed(Boolean.parseBoolean(c[2]));
+        currentCase.setCitizenRepresentation(c[3]);
+        currentCase.setNextAppointment(c[4]);
+        currentCase.setGuardianship(c[5]);
+        currentCase.setPersonalHelper(c[6]);
+        currentCase.setPersonalHelperPowerOfAttorney(Boolean.parseBoolean(c[7]));
+        currentCase.setCitizenRights(c[8]);
+        currentCase.setCitizenInformedElectronic(Boolean.parseBoolean(c[9]));
+        currentCase.setConsent(Boolean.parseBoolean(c[10]));
+        currentCase.setConsentType(c[11]);
+        currentCase.setCollectCitizenInfo(c[12]);
+        currentCase.setSpecialCircumstances(c[3]);
+        currentCase.setDifferentCommune(c[14]);
+        
+        
+        return currentCase;
+        
+    }
     
-    
+    public CaseRequest getCaseRequest(int caseRequestID){
+        String[]cr = reader.getCaseRequest(caseRequestID);
+        
+        long l = Long.parseLong(cr[8]);     //CPR
+        int i1 = Integer.parseInt(cr[0]);   //EmployeeID
+        int i2 = Integer.parseInt(cr[1]);   //CaseReqID
+        
+        CaseRequest currentCaseRequest = new CaseRequest(i1,i2 , l);
+        
+        currentCaseRequest.setDescription(cr[2]);
+        currentCaseRequest.setMessageClear(Boolean.parseBoolean(cr[3]));
+        currentCaseRequest.setCarePackageRequested(Boolean.parseBoolean(cr[4]));
+        currentCaseRequest.setRehousingPackageRequested(Boolean.parseBoolean(cr[5]));
+        currentCaseRequest.setRequestPerson(cr[6]);
+        currentCaseRequest.setCitizenInformed(Boolean.parseBoolean(cr[7]));
+        currentCaseRequest.connectCitizen(l, cr[9], cr[10].charAt(0), cr[11], cr[12]);  //cpr, name, gender, birthday, address
+        currentCaseRequest.setCitizenPhoneNr(Integer.parseInt(cr[13]));
+        currentCaseRequest.setCitizenMail(cr[14]);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        return currentCaseRequest;
+        
+    }
+   
     
     
 }
